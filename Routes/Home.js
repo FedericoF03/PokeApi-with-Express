cf477b9d962ea400e4db9d493f56600c73a0050e
FedericoF03 <federicofernandeztec@gmail.com>
2022-10-17 16:56:13 -0300

    Cambiando ruta

--- Routes/Home.js
+++ Routes/Home.js
@@ -6,6 +6,6 @@ const __filename = fileURLToPath(import.meta.url),
     __dirname = path.dirname(__filename),
     Router = express.Router();
 
-Router.get('/', (req, res) => res.sendFile( 'Home.html', { root: path.join(__dirname, '../Public/Views/') } ) )
+Router.get('/home', (req, res) => res.sendFile( 'Home.html', { root: path.join(__dirname, '../Public/Views/') } ) )
 
 export default Router
