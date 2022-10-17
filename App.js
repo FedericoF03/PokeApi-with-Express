04920be3bee74b3de5f5b6ad042b17399abfc376
FedericoF03 <federicofernandeztec@gmail.com>
2022-10-17 17:49:19 -0300

    Modificando para deploy

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import path from 'path'
 import favicon from 'serve-favicon'
 import {fileURLToPath} from 'url';
 import homeRoute from './Routes/Home.js'
-import pokedexRoute from './Routes/Pokedex.js'
-import PokemonRoute from './Routes/Pokemon.js'
+// import pokedexRoute from './Routes/Pokedex.js'
+// import PokemonRoute from './Routes/Pokemon.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
@@ -13,12 +13,12 @@ const port = process.env.PORT || 3000
 app
     .set('view engine', 'pug')
     .set('views', path.join(__dirname, '/Public/views'))
-    .set('trust proxy', true)
+    // .set('trust proxy', true)
     .use(morgan('dev'))
     .use(favicon(__dirname + '/public/Assets/icon.ico'))
     .use(homeRoute)
-    .use(pokedexRoute)
-    .use(PokemonRoute)
+    // .use(pokedexRoute)
+    // .use(PokemonRoute)
     
     .use(express.static(path.join(__dirname, '/')))
 app.listen(port, () => {
