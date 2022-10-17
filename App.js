f3964e7bfa0e7cb29738aabd0c31e1992b3efcbe
FedericoF03 <federicofernandeztec@gmail.com>
2022-10-17 18:57:01 -0300

    devolviendo

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import path from 'path'
 import {fileURLToPath} from 'url';
 import homeRoute from './Routes/Home.js'
 import pokedexRoute from './Routes/Pokedex.js'
-// import PokemonRoute from './Routes/Pokemon.js'
+import PokemonRoute from './Routes/Pokemon.js'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
@@ -16,7 +16,7 @@ app
     // .use(favicon(__dirname + '/public/Assets/icon.ico'))
     .use(homeRoute)
     .use(pokedexRoute)
-    // .use(PokemonRoute)
+    .use(PokemonRoute)
     
     .use(express.static(path.join(__dirname, '/')))
 app.listen(port, () => {
