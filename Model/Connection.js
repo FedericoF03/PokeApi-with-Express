55e65c2ca7e9a81ca61f82fb70cb0559eed67359
FedericoF03 <federicofernandeztec@gmail.com>
2022-11-15 22:13:37 -0300

    Se agrego libreria con modificaciones

--- Model/Connection.js
+++ Model/Connection.js
@@ -1,6 +1,5 @@
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv/config.js';
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
 try {
     let connection = await mongoose.connect('mongodb+srv://Marlon:Gk8dr3RXhbS7b8ft@pokemongo.etftzof.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser: true,
