0545c8f6ea292015ab670362aa9f76d8f34353f1
FedericoF03 <federicofernandeztec@gmail.com>
2022-12-03 23:24:47 -0300

    Se arreglo un error con las flechas

--- Public/Js/arrow.js
+++ Public/Js/arrow.js
@@ -1,11 +1,11 @@
 const $arrow = document.getElementById('arrow'),
-{scrollTop} = document.getElementById('conteiner'),
+$conteiner = document.getElementById('conteiner'),
 viewArrowBlock = 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
 viewArrowNone = 'polygon(0 0, 100% 0, 100% 0, 0 0)';
 
 document.addEventListener('click', (e) => {
     if (e.target === $arrow) $conteiner.scrollTop = 0;
 })
-$conteiner.addEventListener('scroll', () => scrollTop > 500 ? $arrow.style.clipPath = viewArrowBlock : $arrow.style.clipPath = viewArrowNone);
+$conteiner.addEventListener('scroll', () => $conteiner.scrollTop > 500 ? $arrow.style.clipPath = viewArrowBlock : $arrow.style.clipPath = viewArrowNone);
 
 export default './arrow.js'
